e398ec303af38061a5f7a8a156d42343b18e76e1
Moshe Eshel <moshe.eshel@kenshoo.com>
2014-10-05 14:10:04 +0300

    Change ContainerState object to be immutable
    This object is a docker response object from a call to docker, in no point
    during the life of docker-client should this object be changed, except by
    querying docker again for state (and getting a new object).
    
    Therefore there is no need to have any setter methods on the objects.
    
    For tests I added json fixtures that are the way the object is being
    created in runtime anyway - a much better way to test than using set
    methods.
    
    For more reasons why immutable is better (where relevant as in this case)
    http://www.javapractices.com/topic/TopicAction.do?Id=29
    
    Also added the ContainerState.Paused and ContainerState.Restarting
    properties.

---
+++ src/test/java/com/spotify/docker/client/messages/ContainerStateTest.java
@@ -0,0 +1,71 @@
+package com.spotify.docker.client.messages;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spotify.docker.client.ObjectMapperProvider;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ContainerStateTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private ObjectMapper objectMapper;
+
+  @Before
+  public void setUp() throws Exception {
+    objectMapper = new ObjectMapperProvider().getContext(ContainerState.class);
+  }
+
+  @Test
+  public void testLoadFromRandomFixture() throws Exception {
+    ContainerState containerState = objectMapper
+        .readValue(fixture("fixtures/container-state-random.json"), ContainerState.class);
+    assertThat(containerState.paused(), is(false));
+    assertThat(containerState.restarting(), is(false));
+    assertThat(containerState.running(), is(true));
+    assertThat(containerState.exitCode(), is(0));
+    assertThat(containerState.pid(), is(27629));
+    assertThat(containerState.startedAt(), is(new Date(1412236798929L)));
+    assertThat(containerState.finishedAt(), is(new Date(-62135769600000L)));
+
+  }
+
+  @Test
+  public void testLoadFromRandomFixtureMissingProperty() throws Exception {
+    ContainerState containerState = objectMapper
+        .readValue(fixture("fixtures/container-state-missing-property.json"), ContainerState.class);
+  }
+
+  @Test
+  public void testLoadInvalidConatainerStateJson() throws Exception {
+    expectedException.expect(JsonMappingException.class);
+    objectMapper.readValue(fixture("fixtures/container-state-invalid.json"), ContainerState.class);
+
+  }
+
+  @Test
+  public void testLoadInvalidJson() throws Exception {
+    expectedException.expect(JsonParseException.class);
+    objectMapper.readValue(fixture("fixtures/invalid.json"), ContainerState.class);
+
+  }
+
+  private static String fixture(String filename) throws IOException {
+    return Resources.toString(Resources.getResource(filename), Charsets.UTF_8).trim();
+  }
+}
