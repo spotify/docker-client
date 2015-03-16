1ba51c4cb061ff0838d85334c98cea9ef45cac7f
Noa Resare <noa@spotify.com>
2015-03-16 10:58:05 +0100

    Rewrote NOTICE, fixed copyright headers
    * Notice now includes details on who owns th respective copyrights.
    * Widened the defintion of a valid copyright header for java files
      using the RegexpHeader feature of checkstyle
    * Added copyright information to files with substantial contributions
      from external people.

--- src/main/java/com/spotify/docker/client/ExecNotFoundException.java
+++ src/main/java/com/spotify/docker/client/ExecNotFoundException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Spotify AB.
+ * Copyright (c) 2014 Oleg Poleshuk.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
