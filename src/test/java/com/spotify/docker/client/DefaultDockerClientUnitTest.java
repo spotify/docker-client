0563ff095dc266e5e83099f69d4e8ba1c2482712
Matthieu Baechler <matthieu.baechler@gmail.com>
2015-03-16 20:10:55 +0100

    spotify issue #138 wrap long lines

--- src/test/java/com/spotify/docker/client/DefaultDockerClientUnitTest.java
+++ src/test/java/com/spotify/docker/client/DefaultDockerClientUnitTest.java
@@ -9,25 +9,29 @@ public class DefaultDockerClientUnitTest {
 
   @Test
   public void testHostForUnixSocket() {
-    DefaultDockerClient client = DefaultDockerClient.builder().uri(DefaultDockerClient.DEFAULT_UNIX_ENDPOINT).build();
+    DefaultDockerClient client = DefaultDockerClient.builder()
+        .uri(DefaultDockerClient.DEFAULT_UNIX_ENDPOINT).build();
     assertThat(client.getHost(), equalTo("localhost"));
   }
   
   @Test
   public void testHostForLocalHttps() {
-    DefaultDockerClient client = DefaultDockerClient.builder().uri("https://localhost:2375").build();
+    DefaultDockerClient client = DefaultDockerClient.builder()
+        .uri("https://localhost:2375").build();
     assertThat(client.getHost(), equalTo("localhost"));
   }
   
   @Test
   public void testHostForFQDNHttps() {
-    DefaultDockerClient client = DefaultDockerClient.builder().uri("https://perdu.com:2375").build();
+    DefaultDockerClient client = DefaultDockerClient.builder()
+        .uri("https://perdu.com:2375").build();
     assertThat(client.getHost(), equalTo("perdu.com"));
   }
   
   @Test
   public void testHostForIPHttps() {
-    DefaultDockerClient client = DefaultDockerClient.builder().uri("https://192.168.53.103:2375").build();
+    DefaultDockerClient client = DefaultDockerClient.builder()
+        .uri("https://192.168.53.103:2375").build();
     assertThat(client.getHost(), equalTo("192.168.53.103"));
   }
 }
