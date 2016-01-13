c7652c2fd2bf02220100c14fa7e734c8bf8f87b4
Marco Lotz <marco-aurelio.bar.lotz@hp.com>
2016-01-13 11:24:42 +0000

    matched google style

--- src/main/java/com/spotify/docker/client/ObjectMapperProvider.java
+++ src/main/java/com/spotify/docker/client/ObjectMapperProvider.java
@@ -1,16 +1,20 @@
 /*
  * Copyright (c) 2014 Spotify AB.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 
 package com.spotify.docker.client;
 
@@ -46,79 +50,81 @@ import org.slf4j.LoggerFactory;
 @Produces(MediaType.APPLICATION_JSON)
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
 
-    private static final Logger log = LoggerFactory.getLogger(ObjectMapperProvider.class);
+  private static final Logger log = LoggerFactory.getLogger(ObjectMapperProvider.class);
 
-    private static final Function<? super Object, ?> VOID_VALUE = new Function<Object, Object>() {
-        @Override
-        public Object apply(final Object input) {
-            return null;
-        }
-    };
-
-    private static final SimpleModule MODULE = new SimpleModule();
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
-    static {
-        try {
-            MODULE.addSerializer(Set.class, new SetSerializer());
-            MODULE.addDeserializer(Set.class, new SetDeserializer());
-            MODULE.addSerializer(ImmutableSet.class, new ImmutableSetSerializer());
-            MODULE.addDeserializer(ImmutableSet.class, new ImmutableSetDeserializer());
-            OBJECT_MAPPER.registerModule(new GuavaModule());
-            OBJECT_MAPPER.registerModule(MODULE);
-            OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-            OBJECT_MAPPER.setDateFormat(new DockerDateFormat());
-        } catch (Throwable t) {
-            log.error("Failure during static initialization", t);
-            throw t;
-        }
+  private static final Function<? super Object, ?> VOID_VALUE = new Function<Object, Object>() {
+    @Override
+    public Object apply(final Object input) {
+      return null;
     }
+  };
+
+  private static final SimpleModule MODULE = new SimpleModule();
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  static {
+    try {
+      MODULE.addSerializer(Set.class, new SetSerializer());
+      MODULE.addDeserializer(Set.class, new SetDeserializer());
+      MODULE.addSerializer(ImmutableSet.class, new ImmutableSetSerializer());
+      MODULE.addDeserializer(ImmutableSet.class, new ImmutableSetDeserializer());
+      OBJECT_MAPPER.registerModule(new GuavaModule());
+      OBJECT_MAPPER.registerModule(MODULE);
+      OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+      OBJECT_MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+      OBJECT_MAPPER.setDateFormat(new DockerDateFormat());
+    } catch (Throwable t) {
+      log.error("Failure during static initialization", t);
+      throw t;
+    }
+  }
+
+  @Override
+  public ObjectMapper getContext(Class<?> type) {
+    return OBJECT_MAPPER;
+  }
+
+  static ObjectMapper objectMapper() {
+    return OBJECT_MAPPER;
+  }
+
+  private static class SetSerializer extends JsonSerializer<Set> {
 
     @Override
-    public ObjectMapper getContext(Class<?> type) {
-        return OBJECT_MAPPER;
+    public void serialize(final Set value, final JsonGenerator jgen,
+        final SerializerProvider provider) throws IOException {
+      final Map map = (value == null) ? null : Maps.asMap(value, VOID_VALUE);
+      OBJECT_MAPPER.writeValue(jgen, map);
     }
+  }
 
-    static ObjectMapper objectMapper() {
-        return OBJECT_MAPPER;
+  private static class SetDeserializer extends JsonDeserializer<Set> {
+
+    @Override
+    public Set<?> deserialize(final JsonParser jp, final DeserializationContext ctxt)
+        throws IOException {
+      final Map map = OBJECT_MAPPER.readValue(jp, Map.class);
+      return (map == null) ? null : map.keySet();
     }
+  }
 
-    private static class SetSerializer extends JsonSerializer<Set> {
+  private static class ImmutableSetSerializer extends JsonSerializer<ImmutableSet> {
 
-        @Override
-        public void serialize(final Set value, final JsonGenerator jgen, final SerializerProvider provider)
-                throws IOException {
-            final Map map = (value == null) ? null : Maps.asMap(value, VOID_VALUE);
-            OBJECT_MAPPER.writeValue(jgen, map);
-        }
+    @Override
+    public void serialize(final ImmutableSet value, final JsonGenerator jgen,
+        final SerializerProvider provider) throws IOException {
+      final Map map = (value == null) ? null : Maps.asMap(value, VOID_VALUE);
+      OBJECT_MAPPER.writeValue(jgen, map);
     }
+  }
 
-    private static class SetDeserializer extends JsonDeserializer<Set> {
+  private static class ImmutableSetDeserializer extends JsonDeserializer<ImmutableSet> {
 
-        @Override
-        public Set<?> deserialize(final JsonParser jp, final DeserializationContext ctxt) throws IOException {
-            final Map map = OBJECT_MAPPER.readValue(jp, Map.class);
-            return (map == null) ? null : map.keySet();
-        }
+    @Override
+    public ImmutableSet<?> deserialize(final JsonParser jp, final DeserializationContext ctxt)
+        throws IOException {
+      final Map map = OBJECT_MAPPER.readValue(jp, Map.class);
+      return (map == null) ? null : ImmutableSet.copyOf(map.keySet());
     }
-
-    private static class ImmutableSetSerializer extends JsonSerializer<ImmutableSet> {
-
-        @Override
-        public void serialize(final ImmutableSet value, final JsonGenerator jgen, final SerializerProvider provider)
-                throws IOException {
-            final Map map = (value == null) ? null : Maps.asMap(value, VOID_VALUE);
-            OBJECT_MAPPER.writeValue(jgen, map);
-        }
-    }
-
-    private static class ImmutableSetDeserializer extends JsonDeserializer<ImmutableSet> {
-
-        @Override
-        public ImmutableSet<?> deserialize(final JsonParser jp, final DeserializationContext ctxt) throws IOException {
-            final Map map = OBJECT_MAPPER.readValue(jp, Map.class);
-            return (map == null) ? null : ImmutableSet.copyOf(map.keySet());
-        }
-    }
+  }
 }
