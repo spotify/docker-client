412ce20c3a3078c38e857ec99cb7201e4524e2c1
David Xia <dxia@spotify.com>
2016-01-16 15:11:52 -0500

    Override equals() and hashCode() for BuildParam

--- src/main/java/com/spotify/docker/client/DockerClient.java
+++ src/main/java/com/spotify/docker/client/DockerClient.java
@@ -515,6 +515,31 @@ public interface DockerClient extends Closeable {
     public static BuildParam pullNewerImage() {
       return create("pull", "true");
     }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+
+      BuildParam that = (BuildParam) o;
+
+      if (name != null ? !name.equals(that.name) : that.name != null) {
+        return false;
+      }
+      return !(value != null ? !value.equals(that.value) : that.value != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+      int result = name != null ? name.hashCode() : 0;
+      result = 31 * result + (value != null ? value.hashCode() : 0);
+      return result;
+    }
   }
 
   /**
