d546ea7be36da0e437c98aed5887fcafc2c034f8
Matt Brown <mattbrown@spotify.com>
2016-03-22 15:34:22 -0400

    consistent setup of DOCKER_OPTS

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-show_help() {
-  echo "Usage: $0 <command>"
-  echo "Commands: install_docker, dump_docker_config"
-}
-
 if [[ -z $1 ]]; then
-  show_help
+  "I need a command!"
   exit 1
 fi
 
@@ -30,6 +25,9 @@ case "$1" in
     apt-cache policy docker-engine
     sudo apt-get -q -y install docker-engine=$DOCKER_VERSION* "linux-image-extra-$(uname -r)"
 
+    # set DOCKER_OPTS to make sure docker listens on the ports we intend
+    echo 'DOCKER_OPTS="-D=true -H=unix:///var/run/docker.sock -H=tcp://127.0.0.1:2375"' | sudo tee -a /etc/default/docker
+
     if [[ "$DOCKER_VERSION" =~ ^1\.6\..* ]]; then
       # docker-engine 1.6.x packages don't seem to have the upstart job config,
       # so write it ourselves
@@ -42,27 +40,34 @@ limit nproc 524288 1048576
 respawn
 kill timeout 20
 script
-	DOCKER=/usr/bin/$UPSTART_JOB
-	DOCKER_OPTS="-D=true -H=unix:///var/run/docker.sock -H=tcp://127.0.0.1:2375"
-	exec "$DOCKER" -d $DOCKER_OPTS
+  DOCKER=/usr/bin/$UPSTART_JOB
+  DOCKER_OPTS=
+  if [ -f /etc/default/$UPSTART_JOB ]; then
+    . /etc/default/$UPSTART_JOB
+  fi
+  exec "$DOCKER" -d $DOCKER_OPTS
 end script
 # Don'"'"'t emit "started" event until docker.sock is ready.
 # See https://github.com/docker/docker/issues/6647
 post-start script
-	DOCKER_OPTS="-D=true -H=unix:///var/run/docker.sock -H=tcp://127.0.0.1:2375"
-	while ! [ -e /var/run/docker.sock ]; do
-		initctl status $UPSTART_JOB | grep -qE "(stop|respawn)/" && exit 1
-		echo "Waiting for /var/run/docker.sock"
-		sleep 0.1
-	done
-	echo "/var/run/docker.sock is up"
+  DOCKER_OPTS=
+  if [ -f /etc/default/$UPSTART_JOB ]; then
+    . /etc/default/$UPSTART_JOB
+  fi
+  while ! [ -e /var/run/docker.sock ]; do
+    initctl status $UPSTART_JOB | grep -qE "(stop|respawn)/" && exit 1
+    echo "Waiting for /var/run/docker.sock"
+    sleep 0.1
+  done
+  echo "/var/run/docker.sock is up"
 end script
       ' | sudo tee /etc/init/docker.conf
       sudo cat /etc/init/docker.conf
+      # need to start the service since we just wrote the upstart config
       sudo start docker
     else
-      # set DOCKER_OPTS to make sure docker listens on the ports we intend
-	    echo 'DOCKER_OPTS="-D=true -H=unix:///var/run/docker.sock -H=tcp://127.0.0.1:2375"' | sudo tee -a /etc/default/docker
+      # restart the service for the /etc/default/docker change we made after
+      # installing the package
       sudo restart docker
     fi
 
