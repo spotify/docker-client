7dea94012ed719e1d006a221907ca6effc5754ab
deleSerna <nadeeshtv@gmail.com>
2016-06-18 01:16:57 +0530

    Reverted version.buildTime to String
    Type of version.buildTime reverted from Date  to String

--- src/main/java/com/spotify/docker/client/messages/Version.java
+++ src/main/java/com/spotify/docker/client/messages/Version.java
@@ -21,14 +21,13 @@ import com.google.common.base.MoreObjects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.Date;
 import java.util.Objects;
 
 public class Version {
 
   @JsonProperty("ApiVersion") private String apiVersion;
   @JsonProperty("Arch") private String arch;
-  @JsonProperty("BuildTime") private Date buildTime;
+  @JsonProperty("BuildTime") private String buildTime;
   @JsonProperty("GitCommit") private String gitCommit;
   @JsonProperty("GoVersion") private String goVersion;
   @JsonProperty("KernelVersion") private String kernelVersion;
@@ -43,8 +42,8 @@ public class Version {
     return arch;
   }
 
-  public Date buildTime() {
-    return buildTime == null ? null : new Date(buildTime.getTime());
+  public String buildTime() {
+    return buildTime;
   }
 
   public String gitCommit() {
