bdf04f607b716a2b958f7018d4b3e333370576d2
David Xia <dxia@spotify.com>
2016-12-30 12:28:24 -0500

    Fix error handling in DefaultDockerClient
    Sometimes the response input stream is closed. Reading from it
    will throw IllegalStateException.

--- src/main/java/com/spotify/docker/client/DefaultDockerClient.java
+++ src/main/java/com/spotify/docker/client/DefaultDockerClient.java
@@ -2110,7 +2110,13 @@ public class DefaultDockerClient implements DockerClient, Closeable {
   }
 
   private String message(final Response response) {
-    final Readable reader = new InputStreamReader(response.readEntity(InputStream.class), UTF_8);
+    final Readable reader;
+    try {
+      reader = new InputStreamReader(response.readEntity(InputStream.class), UTF_8);
+    } catch (IllegalStateException e) {
+      return null;
+    }
+
     try {
       return CharStreams.toString(reader);
     } catch (IOException ignore) {
