b32e7ba7c36a20a7b68a6d43cc0b57085f87f401
David Xia <dxia@spotify.com>
2017-01-03 14:49:14 -0500

    test

--- src/test/java/com/spotify/docker/client/DefaultDockerClientTest.java
+++ src/test/java/com/spotify/docker/client/DefaultDockerClientTest.java
@@ -2647,8 +2647,6 @@ public class DefaultDockerClientTest {
   @Test
   public void testExec() throws Exception {
     requireDockerApiVersionAtLeast("1.15", "Exec");
-    assumeThat("Only native (libcontainer) driver supports Exec",
-               sut.info().executionDriver(), startsWith("native"));
 
     sut.pull(BUSYBOX_LATEST);
 
@@ -2682,11 +2680,6 @@ public class DefaultDockerClientTest {
   @Test
   public void testExecCreateOnNonRunningContainer() throws Exception {
     requireDockerApiVersionAtLeast("1.15", "Exec");
-    // Execution driver is removed in Docker API >= 1.24
-    if (dockerApiVersionLessThan("1.24")) {
-      assumeThat("Only native (libcontainer) driver supports Exec",
-          sut.info().executionDriver(), startsWith("native"));
-    }
 
     sut.pull(BUSYBOX_LATEST);
 
@@ -2709,8 +2702,6 @@ public class DefaultDockerClientTest {
   @Test
   public void testExecInspect() throws Exception {
     requireDockerApiVersionAtLeast("1.16", "Exec Inspect");
-    assumeThat("Only native (libcontainer) driver supports Exec",
-               sut.info().executionDriver(), startsWith("native"));
 
     sut.pull(BUSYBOX_LATEST);
 
