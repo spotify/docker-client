380aa7fcaa142cf1136c05dcf96dea332ed2b4fd
Matt Brown <mattbrown@spotify.com>
2017-01-13 09:53:39 -0500

    attempt to include HTTP error bodies in exception
    When a HTTP error (4xx, 5xx, IOException) occurs in communciating with
    the Docker Remote API, DefaultDockerClient attempts to read the response
    body (if one exists) and passes it to the DockerRequestException which
    is thrown.
    
    The message field that is passed to the DockerRequestException is unused
    in the "message" passed up to the superclass (which is available via
    `getMessage()` and `Exception.toString()`).
    
    This change would include the "message" passed to DockerRequestException
    in the superclass's message if possible, to help improve troubleshooting
    and debugging.
    
    Also remove the five unused constructors in DockerRequestException.

--- src/main/java/com/spotify/docker/client/exceptions/DockerRequestException.java
+++ src/main/java/com/spotify/docker/client/exceptions/DockerRequestException.java
@@ -29,34 +29,12 @@ public class DockerRequestException extends DockerException {
   private final int status;
   private final String message;
 
-  public DockerRequestException(final String method, final URI uri, final Throwable cause) {
-    this(method, uri, 0, null, cause);
-  }
-
-  public DockerRequestException(final String method, final URI uri,
-                                final int status,
-                                final Throwable cause) {
-    this(method, uri, status, null, cause);
-  }
-
-  public DockerRequestException(final String method, final URI uri) {
-    this(method, uri, 0, null, null);
-  }
-
-  public DockerRequestException(final String method, final URI uri,
-                                final int status) {
-    this(method, uri, status, null, null);
-  }
-
-  public DockerRequestException(final String method, final URI uri,
-                                final int status, final String message) {
-    this(method, uri, status, message, null);
-  }
-
   public DockerRequestException(final String method, final URI uri,
                                 final int status, final String message,
                                 final Throwable cause) {
-    super("Request error: " + method + " " + uri + ": " + status, cause);
+    super("Request error: " + method + " " + uri + ": " + status
+          + ((message != null) ? ", body: " + message : ""),
+        cause);
     this.method = method;
     this.uri = uri;
     this.status = status;
