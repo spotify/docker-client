adb53e6a2f8e699fee06524fdb0d6d6f7e7b3e2c
krizsan <ivan.krizsan@gmail.com>
2017-01-28 11:40:26 +0100

    Added missing headers and deleted unused class.

--- src/test/java/com/spotify/docker/it/IvanDockerContainer.java
+++ src/test/java/com/spotify/docker/it/IvanDockerContainer.java
@@ -1,3 +1,24 @@
+/*-
+ * -\-\-
+ * docker-client
+ * --
+ * Copyright (C) 2016 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+*
+ *      http://www.apache.org/licenses/LICENSE-2.0
+*
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+
 package com.spotify.docker.it;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -32,8 +53,7 @@ import org.slf4j.LoggerFactory;
  */
 public class IvanDockerContainer {
   /* Constant(s): */
-  private static final Logger LOGGER =
-      LoggerFactory.getLogger(DockerContainer.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(DockerContainer.class);
 
   /* Instance variable(s): */
   protected DockerClient dockerClient;
@@ -42,8 +62,7 @@ public class IvanDockerContainer {
   protected String dockerContainerId;
   protected LogStream containerLogStream;
 
-  public IvanDockerContainer(final DockerClient inDockerClient,
-                             final String inDockerImageName) {
+  public IvanDockerContainer(final DockerClient inDockerClient, final String inDockerImageName) {
     Preconditions.checkNotNull(inDockerClient);
     Preconditions.checkArgument(StringUtils.isNotEmpty(inDockerImageName));
 
