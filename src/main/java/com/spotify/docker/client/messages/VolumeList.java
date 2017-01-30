b391c9f0cbe2b07a475c048c07db728b35f44990
John Flavin <flavinj@gmail.com>
2017-01-30 10:26:07 -0600

    603 Only copy volumes if non-null

--- src/main/java/com/spotify/docker/client/messages/VolumeList.java
+++ src/main/java/com/spotify/docker/client/messages/VolumeList.java
@@ -47,8 +47,10 @@ public abstract class VolumeList {
   static VolumeList create(
       @JsonProperty("Volumes") final List<Volume> volumes,
       @JsonProperty("Warnings") final List<String> warnings) {
+    final ImmutableList<Volume> volumesCopy = volumes == null
+                                               ? null : ImmutableList.copyOf(volumes);
     final ImmutableList<String> warningsCopy = warnings == null
                                                ? null : ImmutableList.copyOf(warnings);
-    return new AutoValue_VolumeList(ImmutableList.copyOf(volumes), warningsCopy);
+    return new AutoValue_VolumeList(volumesCopy, warningsCopy);
   }
 }
