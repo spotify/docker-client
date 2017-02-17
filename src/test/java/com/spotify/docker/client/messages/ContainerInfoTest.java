3ce2f36f64f752520cd83f944ddb87fe775fb26a
David Xia <dxia@spotify.com>
2017-02-17 12:44:49 -0500

    Test ContainerInfo deserialization for API 1.22
    fixes #631

--- src/test/java/com/spotify/docker/client/messages/ContainerInfoTest.java
+++ src/test/java/com/spotify/docker/client/messages/ContainerInfoTest.java
@@ -42,6 +42,11 @@ public class ContainerInfoTest {
   }
 
   @Test
+  public void test1_22() throws Exception {
+    objectMapper.readValue(fixture("fixtures/1.22/containerInfo.json"), ContainerInfo.class);
+  }
+
+  @Test
   public void test1_24() throws Exception {
     objectMapper.readValue(fixture("fixtures/1.24/containerInfo.json"), ContainerInfo.class);
   }
