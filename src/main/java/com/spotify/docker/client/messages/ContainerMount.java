581654cfc07c95464a36db8a03f5c1846180c057
John Flavin <flavinj@gmail.com>
2017-04-03 12:22:56 -0500

    Add new fields to ContainerMount

--- src/main/java/com/spotify/docker/client/messages/ContainerMount.java
+++ src/main/java/com/spotify/docker/client/messages/ContainerMount.java
@@ -28,10 +28,20 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 
+import javax.annotation.Nullable;
+
 
 @AutoValue
 @JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, setterVisibility = NONE)
 public abstract class ContainerMount {
+
+  @Nullable
+  @JsonProperty("Type")
+  public abstract String type();
+
+  @Nullable
+  @JsonProperty("Name")
+  public abstract String name();
 
   @JsonProperty("Source")
   public abstract String source();
@@ -39,18 +49,30 @@ public abstract class ContainerMount {
   @JsonProperty("Destination")
   public abstract String destination();
 
+  @Nullable
+  @JsonProperty("Driver")
+  public abstract String driver();
+
   @JsonProperty("Mode")
   public abstract String mode();
 
   @JsonProperty("RW")
   public abstract Boolean rw();
 
+  @Nullable
+  @JsonProperty("Propagation")
+  public abstract String propagation();
+
   @JsonCreator
   static ContainerMount create(
+      @JsonProperty("Type") final String type,
+      @JsonProperty("Name") final String name,
       @JsonProperty("Source") final String source,
       @JsonProperty("Destination") final String destination,
+      @JsonProperty("Driver") final String driver,
       @JsonProperty("Mode") final String mode,
-      @JsonProperty("RW") final Boolean rw) {
-    return new AutoValue_ContainerMount(source, destination, mode, rw);
+      @JsonProperty("RW") final Boolean rw,
+      @JsonProperty("Propagation") final String propagation) {
+    return new AutoValue_ContainerMount(type, name, source, destination, driver, mode, rw, propagation);
   }
 }
