d71e9709d6a20ee7a397f161ef84c302338a7bbf
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2017-05-16 15:11:35 +0200

    checkstyle and imports

--- src/test/java/com/spotify/docker/client/ConnectionPoolTest.java
+++ src/test/java/com/spotify/docker/client/ConnectionPoolTest.java
@@ -3,14 +3,13 @@
  * docker-client
  * --
  * Copyright (C) 2016 Spotify AB
- * Copyright (C) 2016 Thoughtworks, Inc
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +26,9 @@ import com.spotify.docker.client.exceptions.DockerCertificateException;
 import com.spotify.docker.client.exceptions.DockerException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
 import org.hamcrest.MatcherAssert;
