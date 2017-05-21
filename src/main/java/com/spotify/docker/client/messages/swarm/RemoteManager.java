0a4967f9cf5f3e660b2853f5d2a1fd2e30980bb6
Tiago Alves <tiagoalves83@gmail.com>
2017-05-21 20:16:23 -0300

    SwarmInfo

---
+++ src/main/java/com/spotify/docker/client/messages/swarm/RemoteManager.java
@@ -0,0 +1,47 @@
+/*-
+ * -\-\-
+ * docker-client
+ * --
+ * Copyright (C) 2016 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.docker.client.messages.swarm;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, setterVisibility = NONE)
+public abstract class RemoteManager {
+
+  @JsonProperty("Addr")
+  public abstract String addr();
+
+  @JsonProperty("NodeID")
+  public abstract String nodeId();
+
+  @JsonCreator
+  static RemoteManager create(
+      @JsonProperty("Addr") final String addr,
+      @JsonProperty("NodeID") final String nodeId) {
+    return new AutoValue_RemoteManager(addr, nodeId);
+  }
+}
