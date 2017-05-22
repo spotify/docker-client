9cd8837039c90a615cda8ecfbccaa4b383c9e586
Matt Brown <mattbrown@spotify.com>
2017-05-22 16:52:54 -0400

    fix Findbugs warning in RegistryConfigs

--- src/main/java/com/spotify/docker/client/messages/RegistryConfigs.java
+++ src/main/java/com/spotify/docker/client/messages/RegistryConfigs.java
@@ -75,6 +75,9 @@ public abstract class RegistryConfigs {
         new Maps.EntryTransformer<String, RegistryAuth, RegistryAuth>() {
           @Override
           public RegistryAuth transformEntry(final String key, final RegistryAuth value) {
+            if (value == null) {
+              return null;
+            }
             if (value.serverAddress() == null) {
               return value.toBuilder()
                   .serverAddress(key)
