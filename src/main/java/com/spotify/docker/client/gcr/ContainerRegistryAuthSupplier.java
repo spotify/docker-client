9ca4142b51784be0253075fb89181d2e9e80e92c
Matt Brown <mattbrown@spotify.com>
2017-05-23 20:58:32 -0400

    use google-auth-library-oauth2-http for fetching access tokens
    Instead of invoking `gcloud docker` and parsing a docker config file,
    this adds a new ContainerRegistryAuthSupplier which programmatically
    fetches access tokens for a Google service or user account loaded for a
    credentials file.
    
    The benefit of this implementation is that any arbitrary account can be
    used without having to be registered/added to the `gcloud` CLI - the
    supplier just needs to be pointed at a file containing the credentials
    for the account to be used. There is also no need for gcloud to be
    available on the helios agent.
    
    This support should probably be pulled into a new library at some point,
    something like `docker-client-gcr`, so that non-GCR users of
    docker-client don't have to have this additional logic pulled in. For
    now, the Google library needed for GCR support is marked as `optional`
    in the pom.xml so users have to make sure to add the dependency
    themselves.

---
+++ src/main/java/com/spotify/docker/client/gcr/ContainerRegistryAuthSupplier.java
@@ -0,0 +1,242 @@
+/*-
+ * -\-\-
+ * docker-client
+ * --
+ * Copyright (C) 2016 - 2017 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.docker.client.gcr;
+
+import com.google.api.client.util.Clock;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.auth.oauth2.UserCredentials;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.RegistryAuth;
+import com.spotify.docker.client.messages.RegistryAuthSupplier;
+import com.spotify.docker.client.messages.RegistryConfigs;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A RegistryAuthSupplier for getting access tokens from a Google Cloud Platform service or user
+ * account. This implementation uses the google-auth-library-oauth2-http library to get an access
+ * token given an account's credentials, and will refresh the token if it expires within a
+ * configurable amount of time.
+ * <p>
+ * To construct a new instance, use
+ * {@link ContainerRegistryAuthSupplier#fromStream(InputStream)} to get a {@link Builder}.
+ * The scopes used to fetch an access token and the minimum expiry time can be configured before
+ * calling <code>builder.build()</code>.</p>
+ */
+public class ContainerRegistryAuthSupplier implements RegistryAuthSupplier {
+
+  private static final Logger log = LoggerFactory.getLogger(ContainerRegistryAuthSupplier.class);
+
+  // the list returned by `gcloud docker -a`
+  // this may change in the future, and we can't know all values - but should cover most use cases
+  private static final Set<String> GCR_REGISTRIES = ImmutableSet.of(
+      "gcr.io",
+      "us.gcr.io",
+      "eu.gcr.io",
+      "asia.gcr.io",
+      "b.gcr.io",
+      "bucket.gcr.io",
+      "l.gcr.io",
+      "launcher.gcr.io",
+      "appengine.gcr.io",
+      "us-mirror.gcr.io",
+      "eu-mirror.gcr.io",
+      "asia-mirror.gcr.io",
+      "mirror.gcr.io"
+  );
+
+  /**
+   * Constructs a ContainerRegistryAuthSupplier for the account with the given credentials.
+   * The access tokens are scoped to devstorage.read_only only.
+   */
+  public static Builder fromStream(final InputStream credentialsStream)
+      throws IOException {
+    return new Builder(credentialsStream);
+  }
+
+  public static class Builder {
+
+    private final InputStream credentialsStream;
+
+    private Collection<String> scopes =
+        ImmutableList.of("https://www.googleapis.com/auth/devstorage.read_only");
+
+    private long minimumExpiryMillis = TimeUnit.MINUTES.toMillis(1);
+
+    private Clock clock = Clock.SYSTEM;
+
+    private final CredentialRefresher refresher = new DefaultCredentialRefresher();
+
+    public Builder(final InputStream credentialsStream) {
+      this.credentialsStream = credentialsStream;
+    }
+
+    public Builder withScopes(Collection<String> scopes) {
+      this.scopes = scopes;
+      return this;
+    }
+
+    public Builder withMinimumExpiry(long duration, TimeUnit timeUnit) {
+      this.minimumExpiryMillis = TimeUnit.MILLISECONDS.convert(duration, timeUnit);
+      return this;
+    }
+
+    @VisibleForTesting
+    public Builder withClock(Clock clock) {
+      this.clock = clock;
+      return this;
+    }
+
+    public ContainerRegistryAuthSupplier build() throws IOException {
+      final GoogleCredentials credentials = GoogleCredentials
+          .fromStream(credentialsStream)
+          .createScoped(scopes);
+
+      // log some sort of identifier for the credentials, which requires looking at the
+      // instance type
+      if (credentials instanceof ServiceAccountCredentials) {
+        final String clientEmail = ((ServiceAccountCredentials) credentials).getClientEmail();
+        log.info("loaded credentials for service account with clientEmail={}", clientEmail);
+      } else if (credentials instanceof UserCredentials) {
+        final String clientId = ((UserCredentials) credentials).getClientId();
+        log.info("loaded credentials for user account with clientId={}", clientId);
+      }
+
+      return new ContainerRegistryAuthSupplier(credentials, clock, minimumExpiryMillis, refresher);
+    }
+  }
+
+  /**
+   * Refreshes a GoogleCredentials instance. This only exists for testing - we cannot mock calls to
+   * {@link GoogleCredentials#refresh()} as the method is final.
+   */
+  @VisibleForTesting
+  interface CredentialRefresher {
+
+    void refresh(GoogleCredentials credentials) throws IOException;
+  }
+
+  private static class DefaultCredentialRefresher implements CredentialRefresher {
+
+    @Override
+    public void refresh(final GoogleCredentials credentials) throws IOException {
+      credentials.refresh();
+    }
+  }
+
+  private final GoogleCredentials credentials;
+  // TODO (mbrown): change to java.time.Clock once on Java 8
+  private final Clock clock;
+  private final long minimumExpiryMillis;
+  private final CredentialRefresher credentialRefresher;
+
+  @VisibleForTesting
+  ContainerRegistryAuthSupplier(
+      final GoogleCredentials credentials,
+      final Clock clock,
+      final long minimumExpiryMillis,
+      final CredentialRefresher credentialRefresher) {
+
+    this.credentials = credentials;
+    this.clock = clock;
+    this.minimumExpiryMillis = minimumExpiryMillis;
+    this.credentialRefresher = credentialRefresher;
+  }
+
+  /**
+   * Get an accessToken to use, possibly refreshing the token if it expires within the
+   * minimumExpiryMillis.
+   */
+  private AccessToken getAccessToken() throws DockerException {
+    // there is a small chance of multiple threads calling refresh() many times, but the
+    // GoogleCredentials implementation has thread-safe behavior internally
+    if (needsRefresh(credentials.getAccessToken())) {
+      try {
+        credentialRefresher.refresh(credentials);
+      } catch (IOException e) {
+        // rethrow as DockerException to match the signature of the RegistryAuthSupplier methods
+        throw new DockerException("Could not refresh access token", e);
+      }
+    }
+
+    return credentials.getAccessToken();
+  }
+
+  private boolean needsRefresh(final AccessToken accessToken) {
+    if (accessToken == null) {
+      // has not yet been fetched
+      return true;
+    }
+
+    // refresh the token if it expires "soon"
+    final long expiresIn =
+        credentials.getAccessToken().getExpirationTime().getTime() - clock.currentTimeMillis();
+
+    return expiresIn <= minimumExpiryMillis;
+  }
+
+  @Override
+  public RegistryAuth authFor(final String imageName) throws DockerException {
+    final String[] imageParts = imageName.split("/", 2);
+    if (imageParts.length < 2 || !GCR_REGISTRIES.contains(imageParts[0])) {
+      // not an image on GCR
+      return null;
+    }
+
+    return authForAccessToken(getAccessToken());
+  }
+
+  // see https://cloud.google.com/container-registry/docs/advanced-authentication
+  private RegistryAuth authForAccessToken(final AccessToken accessToken) {
+    return RegistryAuth.builder()
+        .username("oauth2accesstoken")
+        .password(accessToken.getTokenValue())
+        .build();
+  }
+
+  @Override
+  public RegistryAuth authForSwarm() throws DockerException {
+    return authForAccessToken(getAccessToken());
+  }
+
+  @Override
+  public RegistryConfigs authForBuild() throws DockerException {
+    final AccessToken accessToken = getAccessToken();
+
+    final Map<String, RegistryAuth> configs = new HashMap<>(GCR_REGISTRIES.size());
+    for (String serverName : GCR_REGISTRIES) {
+      configs.put(serverName, authForAccessToken(accessToken));
+    }
+    return RegistryConfigs.create(configs);
+  }
+}
---
+++ src/test/java/com/spotify/docker/client/gcr/ContainerRegistryAuthSupplierTest.java
@@ -0,0 +1,174 @@
+/*-
+ * -\-\-
+ * docker-client
+ * --
+ * Copyright (C) 2016 - 2017 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.docker.client.gcr;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.api.client.util.Clock;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.spotify.docker.client.messages.RegistryAuth;
+import com.spotify.docker.client.messages.RegistryConfigs;
+import java.util.concurrent.TimeUnit;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+public class ContainerRegistryAuthSupplierTest {
+
+  private final DateTime expiration = new DateTime(2017, 5, 23, 16, 25);
+  private final String tokenValue = "abc123.foobar";
+
+  // we can't really mock GoogleCredentials since getAccessToken() is a final method (which can't be
+  // mocked). We can construct an instance of GoogleCredentials for a made-up accessToken though.
+  private final AccessToken accessToken = new AccessToken(tokenValue, expiration.toDate());
+  private final GoogleCredentials credentials = new GoogleCredentials(accessToken);
+
+  private final Clock clock = mock(Clock.class);
+  private final int minimumExpirationSecs = 30;
+
+  // we wrap the call to GoogleCredentials.refresh() in this interface because the actual
+  // implementation in the GoogleCredentials class will throw an exception that it isn't
+  // implemented - only subclasses (constructed from real InputStreams containing real credentials)
+  // implement that method.
+  private final ContainerRegistryAuthSupplier.CredentialRefresher refresher = mock(
+      ContainerRegistryAuthSupplier.CredentialRefresher.class);
+
+  private final ContainerRegistryAuthSupplier supplier =
+      new ContainerRegistryAuthSupplier(credentials, clock,
+          TimeUnit.SECONDS.toMillis(minimumExpirationSecs), refresher);
+
+  private static Matcher<RegistryAuth> matchesAccessToken(final AccessToken accessToken) {
+    // we use two featurematchers because a normal CustomTypeSafeMatcher will call
+    // RegistryAuth.toString() in building the failure message, and the toString of that class
+    // purposefully hides sensitive data like the password.
+
+    // username is always the same
+    final String username = "oauth2accesstoken";
+    final String password = accessToken.getTokenValue();
+
+    final Matcher<RegistryAuth> usernameMatcher =
+        new FeatureMatcher<RegistryAuth, String>(is(username), "username", "username") {
+          @Override
+          protected String featureValueOf(final RegistryAuth actual) {
+            return actual.username();
+          }
+        };
+
+    final Matcher<RegistryAuth> passwordMatcher =
+        new FeatureMatcher<RegistryAuth, String>(is(password), "password", "password") {
+          @Override
+          protected String featureValueOf(final RegistryAuth actual) {
+            return actual.password();
+          }
+        };
+
+    return allOf(usernameMatcher, passwordMatcher);
+  }
+
+  @Test
+  public void testAuthForImage_NoRefresh() throws Exception {
+    when(clock.currentTimeMillis())
+        .thenReturn(expiration.minusSeconds(minimumExpirationSecs + 1).getMillis());
+
+    assertThat(supplier.authFor("gcr.io/foobar/barfoo:latest"), matchesAccessToken(accessToken));
+
+    verify(refresher, never()).refresh(credentials);
+  }
+
+  @Test
+  public void testAuthForImage_RefreshNeeded() throws Exception {
+    when(clock.currentTimeMillis())
+        .thenReturn(expiration.minusSeconds(minimumExpirationSecs - 1).getMillis());
+
+    assertThat(supplier.authFor("gcr.io/foobar/barfoo:latest"), matchesAccessToken(accessToken));
+
+    verify(refresher).refresh(credentials);
+  }
+
+  @Test
+  public void testAuthForImage_TokenExpired() throws Exception {
+    when(clock.currentTimeMillis()).thenReturn(expiration.plusMinutes(1).getMillis());
+
+    assertThat(supplier.authFor("gcr.io/foobar/barfoo:latest"), matchesAccessToken(accessToken));
+
+    verify(refresher).refresh(credentials);
+  }
+
+  @Test
+  public void testAuthForImage_NonGcrImage() throws Exception {
+    when(clock.currentTimeMillis())
+        .thenReturn(expiration.minusSeconds(minimumExpirationSecs + 1).getMillis());
+
+    assertThat(supplier.authFor("foobar"), is(nullValue()));
+  }
+
+  @Test
+  public void testAuthForSwarm_NoRefresh() throws Exception {
+    when(clock.currentTimeMillis())
+        .thenReturn(expiration.minusSeconds(minimumExpirationSecs + 1).getMillis());
+
+    assertThat(supplier.authForSwarm(), matchesAccessToken(accessToken));
+
+    verify(refresher, never()).refresh(credentials);
+  }
+
+  @Test
+  public void testAuthForSwarm_RefreshNeeded() throws Exception {
+    when(clock.currentTimeMillis())
+        .thenReturn(expiration.minusSeconds(minimumExpirationSecs - 1).getMillis());
+
+    assertThat(supplier.authForSwarm(), matchesAccessToken(accessToken));
+
+    verify(refresher).refresh(credentials);
+  }
+
+  @Test
+  public void testAuthForBuild_NoRefresh() throws Exception {
+    when(clock.currentTimeMillis())
+        .thenReturn(expiration.minusSeconds(minimumExpirationSecs + 1).getMillis());
+
+    final RegistryConfigs configs = supplier.authForBuild();
+    assertThat(configs.configs().values(), everyItem(matchesAccessToken(accessToken)));
+
+    verify(refresher, never()).refresh(credentials);
+  }
+
+  @Test
+  public void testAuthForBuild_RefreshNeeded() throws Exception {
+    when(clock.currentTimeMillis())
+        .thenReturn(expiration.minusSeconds(minimumExpirationSecs - 1).getMillis());
+
+    final RegistryConfigs configs = supplier.authForBuild();
+    assertThat(configs.configs().values(), everyItem(matchesAccessToken(accessToken)));
+
+    verify(refresher).refresh(credentials);
+  }
+}
