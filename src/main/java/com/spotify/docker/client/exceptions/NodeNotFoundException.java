edfb4288e80e64645c907043abd47addf79f5579
Stephen Sullivan <catalyst7193@gmail.com>
2017-06-01 21:34:58 -0400

    FEATURE: Added an inspectNode method for the docker swarm API
    DEV: Fix File Headers
    DEV: Added @Nullable annotation for docker 1.12.6
    DEV: Added a unit test to DefaultDockerClientUnitTest for inspectNode
    DEV: Added test for 404 NodeNotFoundException
    DEV: Added additional unit tests for error conditions
    DEV: Fixed some whitespace problems
    DEV: Removed redundant null checks in test method
    DEV: Updated @since for listNodes
    DEV: Created new utility method for docker unit tests to enqueue api version
    DEV: Removed comment from enqueueServerApiVersion method
    DEV: Removed inspectNode test per PR discussion

---
+++ src/main/java/com/spotify/docker/client/exceptions/NodeNotFoundException.java
@@ -0,0 +1,39 @@
+/*-
+ * -\-\-
+ * docker-client
+ * --
+ * Copyright (C) 2016 - 2017 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.docker.client.exceptions;
+
+
+public class NodeNotFoundException extends NotFoundException {
+  private final String nodeId;
+
+  public NodeNotFoundException(final String nodeId, final Throwable cause) {
+    super("Node not found: " + nodeId, cause);
+    this.nodeId = nodeId;
+  }
+
+  public NodeNotFoundException(final String nodeId) {
+    this(nodeId, null);
+  }
+
+  public String getNodeId() {
+    return nodeId;
+  }
+}
---
+++ src/main/java/com/spotify/docker/client/messages/swarm/NodeInfo.java
@@ -0,0 +1,130 @@
+/*-
+ * -\-\-
+ * docker-client
+ * --
+ * Copyright (C) 2016 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.docker.client.messages.swarm;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+
+
+import java.util.Date;
+import javax.annotation.Nullable;
+
+
+@AutoValue
+@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, setterVisibility = NONE)
+public abstract class NodeInfo {
+
+  @JsonProperty("ID")
+  public abstract String id();
+
+  @JsonProperty("Version")
+  public abstract Version version();
+
+  @JsonProperty("CreatedAt")
+  public abstract Date createdAt();
+
+  @JsonProperty("UpdatedAt")
+  public abstract Date updatedAt();
+
+  @JsonProperty("Spec")
+  public abstract NodeSpec spec();
+
+  @JsonProperty("Description")
+  public abstract NodeDescription description();
+
+  @JsonProperty("Status")
+  public abstract NodeStatus status();
+
+  @JsonProperty("ManagerStatus")
+  public abstract ManagerStatus managerStatus();
+
+  @JsonCreator
+  public static AutoValue_NodeInfo create(@JsonProperty("ID") final String id,
+                                          @JsonProperty("Version") final Version version,
+                                          @JsonProperty("CreatedAt") final Date createdAt,
+                                          @JsonProperty("UpdatedAt") final Date updatedAt,
+                                          @JsonProperty("Spec") final NodeSpec nodeSpec,
+                                          @JsonProperty("Description")
+                                            final NodeDescription description,
+                                          @JsonProperty("Status")
+                                            final NodeStatus nodeStatus,
+                                          @JsonProperty("ManagerStatus")
+                                            final ManagerStatus managerStatus) {
+
+    return new AutoValue_NodeInfo(id, version, createdAt, updatedAt,
+        nodeSpec, description, nodeStatus, managerStatus);
+  }
+
+  @AutoValue
+  public abstract static class Criteria {
+    /**
+     * Filter by node id.
+     */
+    @Nullable
+    public abstract String nodeId();
+
+    /**
+     * Filter by label.
+     */
+    @Nullable
+    public abstract String label();
+
+    /**
+     * Filter by node name.
+     */
+    @Nullable
+    public abstract String nodeName();
+
+    /**
+     * Filter by node role {manager | worker}.
+     */
+    @Nullable
+    public abstract String nodeRole();
+
+    public static Builder builder() {
+      return new AutoValue_NodeInfo_Criteria.Builder();
+    }
+
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+      public abstract Builder nodeId(String nodeId);
+
+      public abstract Builder label(String label);
+
+      public abstract Builder nodeName(String nodeName);
+
+      public abstract Builder nodeRole(String nodeRole);
+
+      public abstract NodeInfo.Criteria build();
+    }
+  }
+
+
+  public static NodeInfo.Criteria.Builder find() {
+    return AutoValue_NodeInfo_Criteria.builder();
+  }
+}
