08aa6799b698670949aca716fbb6efc8598905f9
Leen Toelen <leen.toelen@tomtom.com>
2017-08-09 08:35:32 +0200

    fix: updated headers on ecr auth provider

--- src/main/java/com/spotify/docker/client/auth/ecr/ContainerRegistryAuthSupplier.java
+++ src/main/java/com/spotify/docker/client/auth/ecr/ContainerRegistryAuthSupplier.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -69,8 +69,8 @@ public class ContainerRegistryAuthSupplier implements RegistryAuthSupplier {
    * <p>
    * The default value for the minimum expiry time of an access token is one minute. When the
    * ContainerRegistryAuthSupplier is asked for a RegistryAuth, it will check if the existing
-   * authorization token for the AWS AuthorizationData expires within this amount of time. If it does, then
-   * the AuthorizationData is refreshed before being returned.
+   * authorization token for the AWS AuthorizationData expires within this amount of time.
+   * If it does, then the AuthorizationData is refreshed before being returned.
    * </p>
    */
   public static class Builder {
