df6643123a278bbb66b14e85928ec0133483ec76
David Xia <dxia@spotify.com>
2017-11-21 20:07:50 -0800

    Fix ContainerStats for exited containers
    Fixes #933

--- src/main/java/com/spotify/docker/client/messages/CpuStats.java
+++ src/main/java/com/spotify/docker/client/messages/CpuStats.java
@@ -29,6 +29,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
+import javax.annotation.Nullable;
 
 @AutoValue
 @JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, setterVisibility = NONE)
@@ -37,18 +38,35 @@ public abstract class CpuStats {
   @JsonProperty("cpu_usage")
   public abstract CpuUsage cpuUsage();
 
+  @Nullable
   @JsonProperty("system_cpu_usage")
   public abstract Long systemCpuUsage();
 
   @JsonProperty("throttling_data")
   public abstract ThrottlingData throttlingData();
 
+  @AutoValue.Builder
+  public abstract static class Builder {
+
+    public abstract Builder cpuUsage(final CpuUsage cpuUsage);
+
+    public abstract Builder systemCpuUsage(final Long systemCpuUsage);
+
+    public abstract Builder throttlingData(final ThrottlingData throttlingData);
+
+    public abstract CpuStats build();
+  }
+
   @JsonCreator
   static CpuStats create(
       @JsonProperty("cpu_usage") final CpuUsage cpuUsage,
       @JsonProperty("system_cpu_usage") final Long systemCpuUsage,
       @JsonProperty("throttling_data") final ThrottlingData throttlingData) {
-    return new AutoValue_CpuStats(cpuUsage, systemCpuUsage, throttlingData);
+    return new AutoValue_CpuStats.Builder()
+        .cpuUsage(cpuUsage)
+        .systemCpuUsage(systemCpuUsage)
+        .throttlingData(throttlingData)
+        .build();
   }
 
   @AutoValue
@@ -57,6 +75,7 @@ public abstract class CpuStats {
     @JsonProperty("total_usage")
     public abstract Long totalUsage();
 
+    @Nullable
     @JsonProperty("percpu_usage")
     public abstract ImmutableList<Long> percpuUsage();
 
@@ -66,14 +85,32 @@ public abstract class CpuStats {
     @JsonProperty("usage_in_usermode")
     public abstract Long usageInUsermode();
 
+    @AutoValue.Builder
+    public abstract static class Builder {
+
+      public abstract Builder totalUsage(final Long totalUsage);
+
+      public abstract Builder percpuUsage(final List<Long> percpuUsage);
+
+      public abstract Builder usageInKernelmode(final Long usageInKernelmode);
+
+      public abstract Builder usageInUsermode(final Long usageInUsermode);
+
+      public abstract CpuUsage build();
+    }
+
     @JsonCreator
     static CpuUsage create(
         @JsonProperty("total_usage") final Long totalUsage,
         @JsonProperty("percpu_usage") final List<Long> perCpuUsage,
         @JsonProperty("usage_in_kernelmode") final Long usageInKernelmode,
         @JsonProperty("usage_in_usermode") final Long usageInUsermode) {
-      return new AutoValue_CpuStats_CpuUsage(totalUsage, ImmutableList.copyOf(perCpuUsage),
-          usageInKernelmode, usageInUsermode);
+      return new AutoValue_CpuStats_CpuUsage.Builder()
+          .totalUsage(totalUsage)
+          .percpuUsage(perCpuUsage)
+          .usageInKernelmode(usageInKernelmode)
+          .usageInUsermode(usageInUsermode)
+          .build();
     }
   }
 
