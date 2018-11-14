9b37e962557b65456ef904d66308be9ae17a7fd8
Jonathan Lafleche <jlafleche@palantir.com>
2018-11-14 10:08:19 +0000

    fix: include repo digests in image info

--- src/test/java/com/spotify/docker/client/messages/ImageInfoTest.java
+++ src/test/java/com/spotify/docker/client/messages/ImageInfoTest.java
@@ -2,14 +2,14 @@
  * -\-\-
  * docker-client
  * --
- * Copyright (C) 2016 Spotify AB
+ * Copyright (C) 2018 Spotify AB
  * --
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@
 package com.spotify.docker.client.messages;
 
 import static com.spotify.docker.FixtureUtil.fixture;
+import static org.hamcrest.Matchers.eq;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -36,6 +37,7 @@ public class ImageInfoTest {
   @Test
   public void test1_24() throws Exception {
     objectMapper.readValue(fixture("fixtures/1.24/imageInfo.json"), ImageInfo.class);
+    assertThat(imageInfo.repoDigests().size(), eq(1));
   }
 
   /**
@@ -46,6 +48,7 @@ public class ImageInfoTest {
     final ImageInfo imageInfo =
         objectMapper.readValue(fixture("fixtures/1.22/imageInfo.json"), ImageInfo.class);
     assertThat(imageInfo.rootFs(), is(nullValue()));
+    assertThat(imageInfo.repoDigests().size(), eq(1));
   }
 
 }
